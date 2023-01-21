3962339bf954cd0b593bf4f207b8eb0843e14c93
YUUJIIE <y-ujiie@outlook.com>
2023-01-21 16:30:33 +0900

    ハッシュタグ機能追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,21 @@ class Question < ApplicationRecord
     validates :title, presence: true
     validates :name, presence: true
     validates :content, presence: true, length: {minimum: 5}
+
+    after_create do
+        hashtags  = self.content.scan(/[#][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+        hashtags.uniq.map do |hashtag|
+            tag = ActsAsTaggableOn::Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+            tag_list << tag
+        end
+    end
+
+    before_update do
+        tag_list.clear
+        hashtags = self.content.scan(/[#][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+        hashtags.uniq.map do |hashtag|
+            tag = ActsAsTaggableOn::Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+            tag_list << tag
+        end
+    end
 end
